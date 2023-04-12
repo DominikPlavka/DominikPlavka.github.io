79d5f20b86997623517b00a17fe8f833beb8b520
Dominik Plavka <domino.plavka@gmail.com>
2023-04-12 17:51:11 +0200

    Added new funcionalities, finalized the behavior of carousel

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const projectData = [
 ];
 
 const slides = [
-  "nodeJS.png",
-  "TS-JS.png",
-  "react.png",
-  "github.png"
+  { image: "nodeJS.png", text: "blabla1" },
+  { image: "TS-JS.png", text: "blabla2" },
+  { image: "react.png", text: "blabla3" },
+  { image: "github.png", text: "blabla4" }
 ];
 
 function App() {
@@ -20,8 +20,8 @@ function App() {
 
       <Navbar />
 
-      <div className="w-4/5 md:w-4x5 lg:w-3/5 justify-center mx-auto my-24 bg-white shadow-myShadow">
-        
+      <div className="max-w-screen-xl w-4/5 md:w-4x5 lg:w-3/5 justify-center mx-auto my-24 bg-white shadow-myShadow">
+
         <div className='md:grid md:grid-cols-2 md:gap-8'>
           <div className='p-12 flex items-center justify-center'>
             <img src="myHead.png" className='w-60 h-auto w-80 lg:h-auto'></img>
@@ -29,7 +29,7 @@ function App() {
           <div className="flex-col p-12 flex md:items-start items-center justify-center">
             <p className="text-2xl">Hello, my name is</p>
             <p className="text-5xl lg:text-7xl font-bold">Dominik</p>
-            <p className="text-2xl mt-12 text-center md:text-left">&#60;junior&#62;&nbsp;Full Stack Dev&nbsp;&#60;&#47;junior&#62;<span className='font-bold animate-ping text-sky-500'>|</span></p>
+            <p className="text-2xl mt-12 text-center md:text-left">&#60;junior&#62;&nbsp;Full Stack Dev&nbsp;&#60;&#47;junior&#62;<span className='font-bold animate-ping text-red-500'>|</span></p>
           </div>
         </div>
 
@@ -49,15 +49,21 @@ function App() {
         </div>
 
         <div id="aboutMe" className='flex flex-col order-last md:order-first md:grid md:grid-cols-2 md:gap-8'>
-        <div className='max-w-lg'>
-          <Carousel autoSlide={true}>
-            {slides.map((slide, index) => <img key={index} src={slide}></img>)}
-          </Carousel>
-        </div>
-        </div>
+          
+          <div className='p-12 flex flex-col order-first md:order-last'>
+            <h2 className="text-3xl lg:text-4xl">Skills</h2>
+            <p className='mt-5'>My tech skills include various</p>
+            <p className='mt-5'>This experience could be very useful in web development as well, because I participated in web/graphic design and I created basic webpages for various clients.</p>
+          </div>
 
+          <div className='flex items-center justify-center max-w-lg'>
+            <Carousel autoSlide={true} slides={slides} />
+          </div>
+
+        </div>
       </div>
-      <div className = 'mb-10 ml-20 mr-20'>
+
+      <div className='mb-10 ml-20 mr-20'>
         <p className='text-white text-center'>Whole page hard-coded &#40;no external packages used, except <a href="https://react-icons.github.io/react-icons/" target="_blank"><span className='text-sky-400'>react-icons</span></a>&#41;. Latest version deployed 12.4.2023</p>
       </div>
     </div>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,67 @@
 import { useState } from 'react';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
+import { useEffect } from 'react';
 
-const Carousel = ({ children: slides }) => {
+
+const Carousel = ({ slides, autoSlide=false,  }) => {
+
+    const autoSlideInterval=3000;
 
     const [current, setCurrent] = useState(0);
 
-    const next = () => {
+    const previous = () => {
         setCurrent(curr => {
             return (curr === 0 ? slides.length - 1 : curr - 1)
         })
     }
 
-    const previous = () => {
+    const next = () => {
         setCurrent(curr => {
             return (curr === slides.length - 1 ? 0 : curr + 1)
         })
     }
 
+    useEffect (() => {
+        if (!autoSlide) return
+            const slideInterval = setInterval(next, autoSlideInterval)
+            return () => clearInterval(slideInterval)
+        
+    }, [])
+
     return (
+
         <div className="overflow-hidden relative">
+            
             <div className="flex transition-transform ease-out duration-1000" style={{ transform: `translateX(-${current * 100}%)` }}>
-                {slides}
+                {slides.map((data, index) =>
+                        <img src={`${data.image}`} />
+                )}
+            
+            </div>
+
+            <div className="absolute inset-0 flex intems-center justify-between p-6">
+                <button onClick={previous}>
+                    <AiOutlineArrowLeft />
+                </button>
+                <button onClick={next}>
+                    <AiOutlineArrowRight />
+                </button>
+            </div>
+
+            <div className='absolute bottom-4 right-0 left-0'>
+                <div className='flex items-center justify-center gap-2'>
+                    {slides.map((xxx, index) =>
+                        <div key={index}>
+                            <div className={`transition-all w-3 h-3 bg-gray-500 rounded-full ${current === index ? "p-2" : "bg-opacity-50"}`} />
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            {/*<div className="flex transition-transform ease-out duration-1000" style={{ transform: `translateX(-${current * 100}%)` }}>
+                {slides.map((data, index) =>
+                    <img key={index} src={`${data.image}`} />
+                )}
             </div>
 
             <div className="absolute inset-0 flex intems-center justify-between p-6">
@@ -40,7 +81,7 @@ const Carousel = ({ children: slides }) => {
                         </div>
                     )}
                 </div>
-            </div>
+            </div>*/}
 
         </div>
     );
